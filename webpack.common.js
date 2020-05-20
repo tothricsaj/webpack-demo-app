219e3183dacb0a4d871ea1b49668e32dd0d50c63
tothricsaj <tothricsaj@gmail.com>
2020-05-20 20:12:23 +0200

    path is deleted

--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,3 @@
-const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
